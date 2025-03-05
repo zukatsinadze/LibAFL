90daf25a07d4ebfdc5008bf82e82eeeaf2851c8a
Zuka Tsinadze <zurab.tsinadze@epfl.ch>
2025-03-05 11:17:06 +0100

    more printlns

--- libafl/src/fuzzer/mod.rs
+++ libafl/src/fuzzer/mod.rs
@@ -244,8 +244,9 @@ where
         let monitor_timeout = STATS_TIMEOUT_DEFAULT;
         loop {
             println!("Yeey");
+            println!("Yeey");
+            println!("Yeey");
             log::info!("Starting another fuzz_loop");
-            thread::sleep(Duration::from_secs(10)); 
             manager.maybe_report_progress(state, monitor_timeout)?;
             self.fuzz_one(stages, executor, state, manager)?;
         }
